70b2d1a96ae06bdd25e901b5730ef27083bd824d
Willian-Cueva <enriquewillian2@gmail.com>
2022-06-11 22:16:53 -0500

    modificando

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// habla itatix q mas q te cuentas
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
